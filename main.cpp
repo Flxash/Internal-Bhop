fd13c61a7c54fd861172a158aeb4178cf952bcf6
Abdel Yasser <zahabys@gmail.com>
2024-03-05 19:37:30 -0800

    Added memory offset namespaces with 'placeholder' offset values + implmeneted the bhop function

--- main.cpp
+++ main.cpp
@@ -4,10 +4,56 @@
 #include <cstdint>
 #include <thread>
 
+// make sure to update the offsets because csgo is most likeley updated
+namespace offset
+{
+	// client 
+	constexpr std::ptrdiff_t dwLocalPlayer = 0xDB458C;
+	constexpr std::ptrdiff_t dwForceJump = 0x527998C;
+
+	// player
+	constexpr std::ptrdiff_t m_iHealth = 0x100;
+	constexpr std::ptrdiff_t m_fFlags = 0x104;
+}
+
+
 // hack fn
 void BunnyHop(const HMODULE instance) noexcept
 {
+	const auto client = reinterpret_cast<std::uintptr_t>(GetModuleHandle("client.dll"));
 
+	// hack loop
+	while (!GetAsyncKeyState(VK_END))
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+		if (!GetAsyncKeyState(VK_SPACE))
+			continue;
+
+		// get local player
+		const auto localPlayer = *reinterpret_cast<std::uintptr_t*>(client + offset::dwLocalPlayer);
+
+		// is valid
+		if (!localPlayer)
+			continue;
+
+		const auto health = *reinterpret_cast<std::int32_t*>(localPlayer + offset::m_iHealth);
+
+		// is alive
+		if (!health)
+			continue;
+
+		const auto flags = *reinterpret_cast<std::int32_t*>(localPlayer + offset::m_fFlags);
+
+		// on ground
+		(flags & (1 << 0)) ?
+			*reinterpret_cast<std::uintptr_t*>(client + offset::dwForceJump) = 6 : // force jump
+			*reinterpret_cast<std::uintptr_t*>(client + offset::dwForceJump) = 4;  // // reset
+
+	}
+
+	// uninject dll
+	FreeLibraryAndExitThread(instance, 0);
 }
 
 // entry point
